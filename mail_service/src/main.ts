4637c3d861b6678e0fabda8ee89d71f2d8de6a24
Eduardo Backes <eduardo.programacao@hotmail.com>
2025-10-05 05:22:05 -0300

    fix

--- mail_service/src/main.ts
+++ mail_service/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqps://vjxuhnkb:N22_7zvU4T083bseFb8K6ggJhQhm7-wt@whale.rmq.cloudamqp.com/vjxuhnkb'],
+      queue: 'email_queue',
+      queueOptions: {
+        durable: false
+      }
+    }
+  });
+  await app.listen();
 }
 bootstrap();
